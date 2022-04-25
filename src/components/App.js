b93473de37154ac4a3b291003164bc243b44bc53
Fiona Y <fyeung21@gmail.com>
2022-04-24 21:51:03 -0700

    Set up state constants

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,15 @@ import Header from './Header';
 import Display from './Display';
 import Buttons from './Buttons';
 import Footer from './Footer';
+import { useState } from 'react';
 
 const App = () => {
+
+  //store calculations, user input
+  const [calc, setCalc] = useState("");
+
+  //store result after calculation to display on screen
+  const [result, setResult] = useState("");
 
   const handleClick = () => {
     console.log('clicked');
